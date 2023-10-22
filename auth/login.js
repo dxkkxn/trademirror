33fc4943cafb9cc3c37ecd400ff9a1f8ab515d99
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2023-10-22 19:26:48 +0200

    adding auth done by Etienne but putting everything inside a folder called auth.

---
+++ auth/login.js
@@ -0,0 +1,16 @@
+const {createClient} = require("redis")
+require("dotenv").config() // access env variables
+
+const redisClient = createClient({ // config client
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    servername: process.env.REDIS_HOST,
+  },
+})
+(async ()=> {
+  // connect to server
+  await redisClient.connect()
+})()
+
+
---
+++ auth/tests/token.test.js
@@ -0,0 +1,9 @@
+// test/example.test.js
+
+const assert = require('assert');
+
+describe('Example Test', () => {
+  it('should return true', () => {
+    assert.strictEqual(true, true);
+  });
+});
