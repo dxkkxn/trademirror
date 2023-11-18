428522f496ba008cb6eb04b91fddc8d2d00ac528
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2023-11-18 18:07:59 +0100

    Adding microservices to the docker-compose

---
+++ frequent_traders_detection/consumer.py
@@ -0,0 +1,35 @@
+import redis
+from confluent_kafka import Consumer, KafkaException
+import sys
+
+# from kafka import KafkaConsumer
+
+redis_client = redis.Redis(host="redis", port=6379, db=0)
+# consumer = KafkaConsumer('frequent-traders')
+consumer_conf = {"bootstrap.servers": "kafka:29092", "client.id": "my-consumer","group.id":"trading-group"}
+topic = "frequent-traders"
+consumer = Consumer(consumer_conf)
+try:
+    consumer.subscribe([topic])
+    while True:
+        print(consumer)
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaException._PARTITION_EOF:
+                continue
+            else:
+                print(msg.error())
+                break
+        print("Received message: {0}".format(msg.value()))
+        # print("Received message: {0}".format(msg))
+        redis_client.lpush("frequent-trading-wallets", msg.value())
+except KeyboardInterrupt:
+    sys.stderr.write("aborted by user.")
+finally:
+    consumer.close()
+
+
+# for msg in consumer:
+#     print(f"msg received {msg.value}")
