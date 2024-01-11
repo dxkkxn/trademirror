4a5a448ca4114fc54e3935d80fbd26a03868b064
Garrenlus De Souza <garrenlus.de-souza@grenoble-inp.org>
2024-01-11 16:59:50 +0000

    feat: user_balance, user_history and follow_wallet implemented for "default_user"

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from controller import user
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import redis
 import os
 import json
 
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
+
+class ReplicateWallet(BaseModel):
+    wallet_id: str
 
 app = FastAPI()
 redis_db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
@@ -35,6 +39,7 @@ async def get_redis_status():
     except redis.ConnectionError:
         return {"Error Redis not working"}
 
+
 @app.get("/api/latest_transactions")
 async def get_latest_transactions():
     try:
@@ -48,34 +53,66 @@ async def get_latest_transactions():
                 current_balance = int(str(redis_db.hget(wallet, "balance")))
                 previous_balance = int(transaction["current_balance"])
                 btc_price = float(transaction["btc_price"])
-                update_percentage = (current_balance - previous_balance) / previous_balance
+                update_percentage = (
+                    current_balance - previous_balance
+                ) / previous_balance
                 transactions[wallet] = {
                     "wallet": wallet,
                     "current_balance": current_balance,
-                    "balance_update": ("+" if update_percentage >= 0 else "-") + str(update_percentage * 100) + "%",
-                    "btc_price" : btc_price
+                    "balance_update": ("+" if update_percentage >= 0 else "-")
+                    + str(update_percentage * 100)
+                    + "%",
+                    "btc_price": btc_price,
                 }
         return json.dumps(transactions)
     except:
         raise Exception("Failed at getting latest transactions")
 
+
 @app.get("/api/user_balance")
 async def get_user_balance():
     try:
-        pass
+        user_balance = json.loads(
+            str(redis_db.hget("users:user-balance", "default_user"))
+        )
+        fiat_balance = user_balance["fiat"]
+        bitcoin_balance = user_balance["btc"]
+        return json.dumps({"fiat": fiat_balance, "btc": bitcoin_balance})
     except:
-        pass
+        raise Exception("Failed at fetching user balance")
+
 
 @app.get("/api/user_history")
 async def get_user_balance():
     try:
-        pass
+        parsed_transactions = []
+        user_transactions = redis_db.lrange("users:transactions:" + "default_user", 0, -1)
+        for tx in user_transactions:
+            transaction = json.loads(tx)
+
+            operation = str(transaction["op"])
+            amount = float(transaction["value"])
+            previous_balance = int(transaction["current_balance"])
+            btc_price = float(transaction["btc_price"])
+
+            parsed_transactions.append(
+                {
+                    "op": operation,
+                    "previous_balance": previous_balance,
+                    "btc_price": btc_price,
+                    "amount": amount,
+                }
+            )
+        return json.dumps(parsed_transactions)
     except:
-        pass
+        raise Exception("Failed at fetching user balance")
+
 
 @app.post("/api/follow_wallet")
-async def follow_wallet():
+async def follow_wallet(data: ReplicateWallet):
     try:
-        pass
+        redis_db.rpush("users:tracking:default_user", data.wallet_id)
+        message = f"default_user is now tracking {data.wallet_id}"
+        return message
     except:
-        pass
+        raise Exception("Failed at adding wallet to the tracking list of user")
