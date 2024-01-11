776cc604748f1fdac14fe8bc56458c5c530db2bd
Youssef Benjelloun <ybkstudies@gmail.com>
2024-01-11 15:51:14 +0100

    docker compose for ftd finished
    kafka running in host machine and container communicates with kafka in
    host machine

--- frequent-traders-detector/database-driver/main.py
+++ frequent-traders-detector/database-driver/main.py
@@ -3,9 +3,12 @@ import json
 from kafka import KafkaConsumer
 
 redis_client = redis.Redis(host="localhost", port=6379, db=0)
-consumer = KafkaConsumer('frequent-traders',
-                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-
+consumer = KafkaConsumer(
+    "frequent-traders",
+    bootstrap_servers="localhost:9092",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+print("Initialized succesfully")
 for msg in consumer:
     print(f"msg received {msg.value}")
     addr = msg.value['addr']
--- frequent-traders-detector/ftd/main.py
+++ frequent-traders-detector/ftd/main.py
@@ -20,7 +20,7 @@ class TradersDetector:
         # sent or not sent (kafka)
 
         self.producer = KafkaProducer(
-            bootstrap_servers="localhost:9092",
+            bootstrap_servers="127.0.0.1:9092",
             value_serializer=lambda v: json.dumps(v).encode("utf-8"),
         )
 
