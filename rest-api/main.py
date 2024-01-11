0c2d2b1d997ab2d54d7b8ac0b04b5f5a95d0f48a
Garrenlus De Souza <garrenlus.de-souza@grenoble-inp.org>
2024-01-11 18:50:53 +0000

    fix: frequent trader's wallets with no operations are not sent to the front end

--- rest-api/main.py
+++ rest-api/main.py
@@ -43,12 +43,12 @@ async def get_redis_status():
 @app.get("/api/latest_transactions")
 async def get_latest_transactions():
     try:
-        ftw = redis_db.lrange("frequent-trading-wallets", 0, 0)
+        ftw = redis_db.lrange("frequent-trading-wallets", 0, 10)
         transactions = {}
         for wallet in ftw:
-            transactions[wallet] = {}
             last_transaction = redis_db.lrange(wallet + ":op", 0, 0)
             for tx in last_transaction:
+                transactions[wallet] = {}
                 transaction = json.loads(tx)
                 current_balance = int(str(redis_db.hget(wallet, "balance")))
                 previous_balance = int(transaction["current_balance"])
