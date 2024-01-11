df6925020f4f312381fd64f97c2ea6913e113386
langleet <etienne.langlet@grenoble-inp.org>
2024-01-11 19:56:34 +0100

    Displaying global recent transactions (5)

--- frontend/src/pages/Dashboard/components/HistorySection.jsx
+++ frontend/src/pages/Dashboard/components/HistorySection.jsx
@@ -10,10 +10,11 @@ const HistorySection = () => {
 
     // Function to fetch last n transactions
     const fetchHistory = () => {
-    fetch('/api/user_history')
+      fetch('/api/user_history')
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok.');
+                console.log('HistorySection : Network response was not ok.');
+                return null;
             }
             return response.json();
         })
@@ -37,42 +38,59 @@ const HistorySection = () => {
       // polling all relevant data
     }, []);
 
+  try
+  {
     return (
             <CustomCard borderRadius="xl">
                 <Text textStyle="h2" color="black.80">Recent Transactions</Text>
-                  {history == null || Object.keys(history).length === 0 ? (
+                  {history == null || history == {} ? (
                     <Button w="full" mt="6" colorScheme="gray">
                         View All
                     </Button>
                 ) : (
                   <Stack>
-                  {Object.keys(history).map((key) => (
-                            <Flex p="1" key={key} gap="4" w="full">
- 
-                              <Flex justify="space-between" w="full" >
-                                  <Stack >
-                                      <Text textStyle="h6">
-                                          <b>{history[key].time}</b>
-                                      </Text>
-                                      <Text fontSize="sm" color= {history[key].amount.includes('+') ? "green" : "red"}>
-                                          Amount : {history[key].amount}
-                                      </Text>
-                                      <Text textStyle="h6">
-                                          Bitcoin Price : {history[key].btc_price} $
-                                      </Text>
+                    {history.map((transaction) => (
+                        Object.keys(transaction).map((key) => (
+                              <Flex p="1" key={key} gap="4" w="full">
+   
+                                <Flex justify="space-between" w="full" >
+                                    <Stack >
+                                        <Text textStyle="h6">
+                                            <b>{history[key].time}</b>
+                                        </Text>
+                                        <Text fontSize="sm" color= {history[key].amount.includes('+') ? "green" : "red"}>
+                                            Amount : {history[key].amount}
+                                        </Text>
+                                        <Text textStyle="h6">
+                                            Bitcoin Price : {history[key].btc_price} $
+                                        </Text>
 
-                                  </Stack>
+                                    </Stack>
+                                </Flex>
+                                 
                               </Flex>
-                               
-                            </Flex>
-                        ))}
-                        <Button w="full" mt="6" colorScheme="gray">
-                            View All
-                        </Button>
-                    </Stack>
-                )}
+                            )
+                          )
+                        )
+                      )
+                    }
+                    <Button w="full" mt="6" colorScheme="gray">
+                        View All
+                    </Button>
+                  </Stack>
+              )}
             </CustomCard>
         );
-    };
+  }
+  catch 
+  {
+    return (
+      <Button w="full" mt="6" colorScheme="gray">
+          View All
+      </Button>
+    );
+
+  }
+};
 
 export default HistorySection;
