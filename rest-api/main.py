3709517d154112f79c852a65f5dcf7ecaabbb45b
Garrenlus De Souza <garrenlus.de-souza@grenoble-inp.org>
2024-01-11 20:36:37 +0000

    fix: division by zero when wallets with zero funds make a transaction

--- rest-api/main.py
+++ rest-api/main.py
@@ -46,23 +46,35 @@ async def get_latest_transactions():
         ftw = redis_db.lrange("frequent-trading-wallets", 0, 10)
         transactions = []
         for wallet in ftw:
-            last_transactions = redis_db.lrange(wallet + ":op", 0, 0)
+            last_transactions = redis_db.lrange(wallet + ":op", 0, -1)
             for tx in last_transactions:
                 transaction = json.loads(tx)
                 current_balance = int(str(redis_db.hget(wallet, "balance")))
                 previous_balance = int(transaction["current_balance"])
                 btc_price = float(transaction["btc_price"])
-                update_percentage = (
-                    current_balance - previous_balance
-                ) / previous_balance
-                transactions.append({
-                    "wallet": wallet,
-                    "current_balance": current_balance,
-                    "balance_update": ("+" if update_percentage >= 0 else "-")
-                    + str(update_percentage * 100)
-                    + "%",
-                    "btc_price": btc_price,
-                })
+
+                if previous_balance == 0:
+                    if current_balance > 0:
+                        update_percentage = float("inf")
+                    elif current_balance < 0:
+                        update_percentage = float("-inf")
+                    else:
+                        update_percentage = 0
+                else:
+                    update_percentage = (
+                        current_balance - previous_balance
+                    ) / previous_balance
+
+                transactions.append(
+                    {
+                        "wallet": wallet,
+                        "current_balance": current_balance,
+                        "balance_update": ("+" if update_percentage >= 0 else "")
+                        + str(update_percentage * 100)
+                        + "%",
+                        "btc_price": btc_price,
+                    }
+                )
         return json.dumps(transactions)
     except:
         raise Exception("Failed at getting latest transactions")
