287ef7d0806abab7c9198d1016f2f499db36d7cd
Youssef Benjelloun <ybkstudies@gmail.com>
2024-01-12 15:27:22 +0100

    new topic created

--- kafka/create_kafka_topic.sh
+++ kafka/create_kafka_topic.sh
@@ -9,7 +9,8 @@ if [[ $existing_topics == *"$topic"* ]]; then
   echo "Topic '$topic' already exists."
 else
   # Create the topic if it doesn't exist
-  kafka-topics --create --bootstrap-server kafka:29092 --replication-factor 1 --partitions 1 --topic "$topic"
+  kafka-topics --create --bootstrap-server kafka:29092 --replication-factor 1 --partitions 6 --topic "$topic"
+  kafka-topics --create --bootstrap-server kafka:29092 --replication-factor 1 --partitions 6 --topic "transactions"
   echo "Topic '$topic' has been created."
 fi
 
