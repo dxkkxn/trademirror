6b4bb1aee0839f2f1f62ca1e80554ab542106880
Youssef Benjelloun <ybkstudies@gmail.com>
2024-01-24 00:47:37 +0100

    redis start up modified

---
+++ backend/setup_redis.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+redis-server&
+sleep 5
+redis-cli sadd "wallets:replication:wallet" "default_user"
+redis-cli hset "users:balance" "default_user" '{"fiat": 100000, "btc": 1}'
+tail -f /dev/null
