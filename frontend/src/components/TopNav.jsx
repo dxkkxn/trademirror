574e1dddef8d0fb5ff594a9aef18ca142d7f4488
langleet <etienne.langlet@grenoble-inp.org>
2024-01-24 14:36:39 +0100

    removing status messages

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -6,54 +6,11 @@ const dbStatusIntervalVal = 10
 
 const TopNav = () => {
 
-  const [apiStatus, setApiStatus] = useState('Loading...');
-  const [dbStatus, setDbStatus] = useState('Loading...');
-  const statusIntervalVal=5;
-  // const apiHost = import.meta.env.VITE_API_URL;
-  // console.log(apiHost)
-
-  useEffect(() => {
-    // Function to fetch API status
-    const fetchApiStatus = () => {
-      fetch('/api/')
-        .then(response => response.json())
-        .then(data => setApiStatus(data))
-        .catch(() => setApiStatus('Error'));
-    };
-
-    // Function to fetch Database status
-    const fetchDbStatus = () => {
-      fetch('/api/redis_status')
-        .then(response => response.json())
-        .then(data => setDbStatus(data))
-        .catch(() => setDbStatus('Error'));
-    };
-
-
-    // Fetch statuses initially
-    fetchApiStatus();
-    fetchDbStatus();
-
-    // Set up polling
-    const statusInterval = setInterval(() => {
-      fetchApiStatus();
-      fetchDbStatus();
-    }, 1000*statusIntervalVal); // polling every 5 seconds
-    
-    console.log(apiStatus, dbStatus)
-
-    // Cleanup function to clear the interval when the component is unmounted
-    return () => clearInterval(statusInterval);
-  }, []);
-
   return (
    <Box>
       <HStack  boxShadow="xl" h="16" justify="space-between" px="20">
      
         <Heading fontSize="24px">Trade Mirror</Heading>
-
-        <Text fontSize="18px">{apiStatus}</Text>
-        <Text fontSize="18px">{dbStatus}</Text>
 
         <Menu>
           <MenuButton ><Icon as={FaSignOutAlt} fontSize="24px"/></MenuButton>
@@ -67,5 +24,6 @@ const TopNav = () => {
     </Box>
   );
 };
+
 
 export default TopNav;
